95a2d41c11d4ac9e8d659b331691bd14dfe8214f
joas <joas@informatik.uni-leipzig.de>
2024-11-26 16:36:14 +0100

    initial architecture draft

---
+++ autoencodix/src/autoencodix/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present joas <joas@informatik.uni-leipzig.de>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ autoencodix/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present joas <joas@informatik.uni-leipzig.de>
+#
+# SPDX-License-Identifier: MIT

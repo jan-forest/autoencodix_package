3a39527753af2410d9a43a5cdf5f2819da8ad615
Jan Ewald <ewald@scadsnb60.informatik.informatik.uni-leipzig.de>
2025-03-28 10:28:53 +0100

    First steps and initial ontix architecture definitions

---
+++ src/autoencodix/modeling/_ontix_architecture.py
@@ -0,0 +1,325 @@
+from typing import Optional, Union, Tuple
+
+import torch
+import torch.nn as nn
+
+from autoencodix.base._base_autoencoder import BaseAutoencoder
+from autoencodix.utils._model_output import ModelOutput
+from autoencodix.utils.default_config import DefaultConfig
+
+from ._layer_factory import LayerFactory
+
+
+class OntixArchitecture(BaseAutoencoder):
+    """
+    Vanilla Autoencoder implementation with separate encoder and decoder construction.
+
+    Attributes
+    ----------
+    self.input_dim : int
+        number of input features
+    self.config: DefaultConfig
+        Configuration object containing model architecture parameters
+    self._encoder: nn.Module
+        Encoder network of the autoencoder
+    self._decoder: nn.Module
+        Decoder network of the autoencoder
+
+    Methods
+    -------
+    _build_network()
+        Construct the encoder and decoder networks via the LayerFactory
+    encode(x: torch.Tensor) -> torch.Tensor
+        Encode the input tensor x
+    decode(x: torch.Tensor) -> torch.Tensor
+        Decode the latent tensor x
+    forward(x: torch.Tensor) -> ModelOutput
+        Forward pass of the model, fills in the reconstruction and latentspace attributes of ModelOutput class.
+    reparametrize(mu: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor
+        Reparameterization trick for VAE
+    """
+
+    def __init__(
+        self, config: Optional[Union[None, DefaultConfig]], input_dim: int, ontologies: tuple
+    ) -> None:
+        """
+        Initialize the Vanilla Autoencoder with the given configuration.
+
+        Parameters
+        ----------
+        config : Optional[Union[None, DefaultConfig]]
+            Configuration object containing model parameters.
+        """
+        if config is None:
+            config = DefaultConfig()
+        self._config = config
+        super().__init__(config, input_dim)
+        self.input_dim = input_dim
+        self._mu: nn.Module
+        self._logvar: nn.Module
+        # create masks for sparse decoder
+        self.ontologies = ontologies
+        self.masks = self._make_masks(config = self._config)
+        self.latent_dim = self.masks[0].shape[1]
+        # populate self.encoder and self.decoder
+        self._build_network()
+        self.apply(self._init_weights)
+        self._decoder.apply(self._positive_dec) # Sparse decoder only has positive weights
+
+        # Apply weight mask to create ontology-based decoder
+        with torch.no_grad():
+            # Check that the decoder has the same number of layers as masks
+            if len(self.masks) != len(self._decoder):
+                raise ValueError(
+                    "Number of masks does not match number of decoder layers"
+                )
+            else:
+                for i, mask in enumerate(self.masks):
+                    self._decoder[i].weight.mul_(mask)
+
+
+    def _make_masks(self, config: DefaultConfig) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Create masks for sparse decoder based on ontology via config
+
+        Parameters
+        ----------
+        config : DefaultConfig
+            Configuration object containing model parameters
+
+        Returns
+        -------
+        Tuple[torch.Tensor, torch.Tensor]
+            Tuple containing the masks for the decoder network
+
+        """
+        # Read ontology from config
+        # self.ontologies 
+        # ont_dic_list = dict()
+        masks = tuple()
+        # x = 1
+        # feature_names are all values in the last ontology layer
+        feature_names = set()
+        for key, values in self.ontologies[-1].items():
+            feature_names.update(values)
+        feature_names = list(feature_names)
+
+        # Enumerate through the ontologies
+        for x, ont_dic in enumerate(self.ontologies):
+            # ont_dic = dict()
+            # sep = "\t"
+            # with open(file_path, "r") as ont_file:
+            #     for line in ont_file:
+            #         id_parent = line.strip().split(sep)[1]
+            #         id_child = line.split(sep)[0]
+
+            #         if id_parent in ont_dic:
+            #             ont_dic[id_parent].append(id_child)
+            #         else:
+            #             ont_dic[id_parent] = list()
+            #             ont_dic[id_parent].append(id_child)
+            # ont_dic_list["lvl_" + str(x)] = dict()
+            # x += 1
+            # Number of keys in ont_dic is prev_lay_dim
+            prev_lay_dim = len(ont_dic.keys())
+            # Number of childs in ont_dic is next_lay_dim
+            next_lay_dim = 0
+            for key in ont_dic:
+                next_lay_dim += len(ont_dic[key])
+
+            # node_list are list of parents from next ontology??
+            if x == len(self.ontologies) - 1:
+                # fixed sort of feature list
+                node_list = feature_names
+            else:
+                node_list = list(self.ontologies[x+1].keys())
+
+            # create masks for sparse decoder
+            mask = torch.zeros(next_lay_dim, prev_lay_dim)
+            p_int = 0
+            if len(node_list) == next_lay_dim:
+                if len(ont_dic.keys()) == prev_lay_dim:
+                    for p_id in ont_dic:
+                        feature_list = ont_dic[p_id]
+                        for f_id in feature_list:
+                            if f_id in node_list:
+                                f_int = node_list.index(f_id)
+                                mask[f_int, p_int] = 1
+
+                        p_int += 1
+                else:
+                    print(
+                        "Mask layer cannot be calculated. Ontology key list does not match previous layer dimension"
+                    )
+                    print("Returning zero mask")
+            else:
+                print(
+                    "Mask layer cannot be calculated. Output layer list does not match next layer dimension"
+                )
+                print("Returning zero mask")
+            
+            masks += (mask,)
+
+        if torch.max(mask) < 1:
+            print(
+                "You provided an ontology with no connections between layers in the decoder. Please check your ontology definition."
+            )
+
+        return masks
+
+    def _build_network(self) -> None:
+        """
+        Construct the encoder and decoder networks.
+
+        Handles cases where `n_layers=0` by skipping the encoder and using only mu/logvar.
+        """
+		#### Encoder copied from varix architecture ####
+        enc_dim = LayerFactory.get_layer_dimensions(
+            feature_dim=self.input_dim,
+            latent_dim=self._config.latent_dim,
+            n_layers=self._config.n_layers,
+            enc_factor=self._config.enc_factor,
+        )
+        #
+
+        # Case 1: No Hidden Layers (Direct Mapping)
+        self._encoder = nn.Sequential()
+        self._mu = nn.Linear(self.input_dim, self._config.latent_dim)
+        self._logvar = nn.Linear(self.input_dim, self._config.latent_dim)
+
+        # Case 2: At Least One Hidden Layer
+        if self._config.n_layers > 0:
+            encoder_layers = []
+            print(enc_dim)
+            for i, (in_features, out_features) in enumerate(
+                zip(enc_dim[:-1], enc_dim[1:])
+            ):
+                # since we add mu and logvar, we will remove the last layer
+                if i == len(enc_dim) - 2:
+                    break
+                encoder_layers.extend(
+                    LayerFactory.create_layer(
+                        in_features=in_features,
+                        out_features=out_features,
+                        dropout_p=self._config.drop_p,
+                        last_layer=False,  # only for decoder relevant
+                    )
+                )
+
+            self._encoder = nn.Sequential(*encoder_layers)
+            self._mu = nn.Linear(enc_dim[-2], self._config.latent_dim)
+            self._logvar = nn.Linear(enc_dim[-2], self._config.latent_dim)
+		#### Encoder copied from varix architecture ####
+
+        # Construct Decoder with Sparse Connections via masks
+        # Decoder dimension is determined by the masks
+        dec_dim = [self.latent_dim] + [mask.shape[0] for mask in self.masks] + [self.input_dim]
+        decoder_layers = []
+        for i, (in_features, out_features) in enumerate(zip(dec_dim[:-1], dec_dim[1:])):
+            last_layer = i == len(dec_dim) - 2
+            decoder_layers.extend(
+                LayerFactory.create_layer(
+                    in_features=in_features,
+                    out_features=out_features,
+                    dropout_p=0, ## No dropout in sparse decoder
+                    last_layer=last_layer,
+                    only_linear=True, ## Only linear layers in sparse decoder
+                )
+            )
+
+        self._decoder = nn.Sequential(*decoder_layers)
+
+    def _positive_dec(self, m):
+        if isinstance(m, nn.Linear):
+            m.weight.data = m.weight.data.clamp(min=0)
+
+    ## Defined in the base class
+    # def _init_weights(self, m):
+    #     if isinstance(m, nn.Linear):
+    #         torch.nn.init.xavier_uniform_(m.weight)
+    #         m.bias.data.fill_(0.01)
+
+    ### Copy from Varix Architecture ###
+    def encode(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Encode the input tensor x
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor
+
+        Returns
+        -------
+        torch.Tensor
+            Encoded tensor
+
+        """
+        latent = x  # for case where n_layers=0
+        if len(self._encoder) > 0:
+            latent = self._encoder(x)
+        mu = self._mu(latent)
+        logvar = self._logvar(latent)
+        # numeric stability
+        logvar = torch.clamp(logvar, 0.01, 20)
+        mu = torch.where(mu < 0.0000001, torch.zeros_like(mu), mu)
+        return mu, logvar
+
+    def reparametrize(self, mu: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor:
+        """
+        Reparameterization trick for VAE
+
+        Parameters:
+            mu : torch.Tensor
+            logvar : torch.Tensor
+
+        Returns:
+            torch.Tensor
+
+        """
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def decode(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Decode the latent tensor x
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Latent tensor
+
+        Returns
+        -------
+        torch.Tensor
+            Decoded tensor
+
+        """
+        return self._decoder(x)
+
+    def forward(self, x: torch.Tensor) -> ModelOutput:
+        """
+        Forward pass of the model, fill
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor
+
+        Returns
+        -------
+        ModelOutput
+            ModelOutput object containing the reconstructed tensor and latent tensor
+
+        """
+        mu, logvar = self.encode(x)
+        z = self.reparametrize(mu, logvar)
+        x_hat = self.decode(z)
+        return ModelOutput(
+            reconstruction=x_hat,
+            latentspace=z,
+            latent_mean=mu,
+            latent_logvar=logvar,
+            additional_info=None,
+        )
---
+++ src/autoencodix/ontix.py
@@ -0,0 +1,167 @@
+from typing import Dict, Optional, Type, Union
+import torch
+import numpy as np
+
+from autoencodix.base._base_dataset import BaseDataset
+from autoencodix.base._base_loss import BaseLoss
+from autoencodix.base._base_pipeline import BasePipeline
+from autoencodix.base._base_trainer import BaseTrainer
+from autoencodix.base._base_visualizer import BaseVisualizer
+from autoencodix.base._base_preprocessor import BasePreprocessor
+from autoencodix.base._base_autoencoder import BaseAutoencoder
+from autoencodix.data._datasetcontainer import DatasetContainer
+from autoencodix.data._datasplitter import DataSplitter
+from autoencodix.data.datapackage import DataPackage
+from autoencodix.data._numeric_dataset import NumericDataset
+from autoencodix.data.general_preprocessor import GeneralPreprocessor
+from autoencodix.evaluate.evaluate import Evaluator
+# from autoencodix.modeling._varix_architecture import VarixArchitecture
+from autoencodix.modeling._ontix_architecture import OntixArchitecture
+from autoencodix.trainers._general_trainer import GeneralTrainer
+from autoencodix.utils._result import Result
+from autoencodix.utils.default_config import DefaultConfig
+from autoencodix.utils._losses import VarixLoss
+from autoencodix.visualize.visualize import Visualizer
+
+## Copy from Varix with ontology addition
+class Ontix(BasePipeline):
+    """
+    Ontix specific version of the BasePipeline class.
+    Inherits preprocess, fit, predict, evaluate, and visualize methods from BasePipeline.
+
+    Attributes
+    ----------
+    preprocessed_data : Optional[DatasetContainer]
+        User data if no datafiles in the config are provided. We expect these to be split and processed.
+    raw_user_data : Optional[DataPackage]
+        We give users the option to populate a DataPacke with raw data i.e. pd.DataFrames, MuData.
+        We will process this data as we would do wit raw files specified in the config.
+    config : Optional[Union[None, DefaultConfig]]
+        Configuration object containing customizations for the pipeline
+    _preprocessor : Preprocessor
+        Preprocessor object to preprocess the input data (maybe custom for Ontix)
+    _visualizer : Visualizer
+        Visualizer object to visualize the model output (maybe custom for Ontix)
+    _trainer : GeneralTrainer
+        Trainer object that trains the model (maybe custom for Ontix)
+    _evaluator : Evaluator
+        Evaluator object that evaluates the model performance or downstream tasks (maybe custom for Ontix)
+    result : Result
+        Result object to store the pipeline results
+    _datasets : Optional[DatasetContainer]
+        Container for train, validation, and test datasets (preprocessed)
+    data_splitter : DataSplitter
+        DataSplitter object to split the data into train, validation, and test sets
+
+    Methods
+    -------
+    all methods from BasePipeline
+
+    sample_latent_space(split: str = "test", epoch: int = -1) -> torch.Tensor
+        Samples new latent space points from the learned distribution.
+
+    """
+
+    def __init__(
+        self,
+		ontologies: tuple, # Addition to Varix, mandotory for Ontix
+        user_data: Optional[Union[DataPackage, DatasetContainer]] = None,
+        trainer_type: Type[BaseTrainer] = GeneralTrainer,
+        dataset_type: Type[BaseDataset] = NumericDataset,
+        model_type: Type[BaseAutoencoder] = OntixArchitecture,
+        loss_type: Type[BaseLoss] = VarixLoss,
+        preprocessor_type: Type[BasePreprocessor] = GeneralPreprocessor,
+        visualizer: Optional[BaseVisualizer] = None,
+        evaluator: Optional[Evaluator] = None,
+        result: Optional[Result] = None,
+        datasplitter_type: Type[DataSplitter] = DataSplitter,
+        custom_splits: Optional[Dict[str, np.ndarray]] = None,
+        config: Optional[DefaultConfig] = None,
+    ) -> None:
+        """Initialize Ontix pipeline with customizable components.
+
+        Some components are passed as types rather than instances because they require
+        data that is only available after preprocessing.
+
+        Parameters
+        ----------
+		ontologies: tuple
+			Tuple of dictionaries containing the ontologies to be used to construct sparse decoder layers.
+        preprocessed_data : Union[np.ndarray, AnnData, pd.DataFrame, DatasetContainer]
+            Input data to be processed
+        trainer_type : Type[BaseTrainer]
+            Type of trainer to be instantiated during fit step, default is GeneralTrainer
+        dataset_type : Type[BaseDataset]
+            Type of dataset to be instantiated post-preprocessing, default is NumericDataset
+        loss_type : Type[BaseLoss], which loss to use for Varix, default is VarixAutoencoderLoss
+        preprocessor : Optional[Preprocessor]
+            For data preprocessing, default creates new Preprocessor
+        visualizer : Optional[Visualizer]
+            For result visualization, default creates new Visualizer
+        evaluator : Optional[Evaluator]
+            For model evaluation, default creates new Evaluator
+        result : Optional[Result]
+            Container for pipeline results, default creates new Result
+        datasplitter_type : Type[DataSplitter], optional
+            Type of splitter to be instantiated during preprocessing, default is DataSplitter
+        custom_splits : Optional[Dict[str, np.ndarray]]
+            Custom train/valid/test split indices
+        config : Optional[DefaultConfig]
+            Configuration for all pipeline components
+        """
+
+        super().__init__(
+            user_data=user_data,
+            dataset_type=dataset_type,
+            trainer_type=trainer_type,
+            model_type=model_type,
+            loss_type=loss_type,
+            preprocessor_type=preprocessor_type,
+            visualizer=visualizer or Visualizer(),
+            evaluator=evaluator or Evaluator(),
+            result=result or Result(),
+            datasplitter_type=datasplitter_type,
+            config=config or DefaultConfig(),
+            custom_split=custom_splits,
+        )
+        self.ontologies = ontologies
+
+    def sample_latent_space(self, split: str = "test", epoch: int = -1) -> torch.Tensor:
+        """
+        Samples new latent space points from the learned distribution.
+        Parameters:
+            split: str - The split to sample from (train, valid, test), default is test
+            epoch: int - The epoch to sample from, default is the last epoch (-1)
+        Returns:
+            z: torch.Tensor - The sampled latent space points
+
+        """
+        if not hasattr(self, "_trainer") or self._trainer is None:
+            raise ValueError("Model is not trained yet. Please train the model first.")
+        if self.result.mus is None or self.result.sigmas is None:
+            raise ValueError("Model has not learned the latent space distribution yet.")
+        mu = self.result.mus.get(split=split, epoch=epoch)
+        logvar = self.result.sigmas.get(split=split, epoch=epoch)
+
+        if not isinstance(mu, np.ndarray):
+            raise TypeError(
+                f"Expected value to be of type numpy.ndarray, got {type(mu)}."
+            )
+        if not isinstance(logvar, np.ndarray):
+            raise TypeError(
+                f"Expected value to be of type numpy.ndarray, got {type(logvar)}."
+            )
+        mu_t = torch.from_numpy(mu)
+        logvar_t = torch.from_numpy(logvar)
+
+        # Move to same device and dtype as model
+        mu_t = mu_t.to(
+            device=self._trainer._model.device, dtype=self._trainer._model.dtype
+        )
+        logvar_t = logvar_t.to(
+            device=self._trainer._model.device, dtype=self._trainer._model.dtype
+        )
+
+        with self._trainer._fabric.autocast(), torch.no_grad():
+            z = self._trainer._model.reparametrize(mu_t, logvar_t)
+            return z

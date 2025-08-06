33e6136d854514e7d000fc0b6548bb31cf13f884
Jan Ewald <ewald@scadsnb60.informatik.informatik.uni-leipzig.de>
2025-08-06 14:00:39 +0200

    Related fixes: model weight vis, printing and example data generation to fix 0.5-je-ontix notebook

--- src/autoencodix/trainers/_ontix_trainer.py
+++ src/autoencodix/trainers/_ontix_trainer.py
@@ -38,7 +38,6 @@ class OntixTrainer(GeneralTrainer):
         """Apply ontology-specific processing after backward pass."""
         # Apply positive weight constraint to decoder
         self._model._decoder.apply(self._model._positive_dec)
-        print("applying ontology masking") 
         # Apply ontology-based weight masking
         with torch.no_grad():
             self._validate_masks()

5cb0cfc5811f91ca4f8b88973cf731d777332ef5
Jan Ewald <ewald@scadsnb60.informatik.informatik.uni-leipzig.de>
2025-10-17 16:44:10 +0200

    Fix in combine script

--- notebooks/combine_and_split_census.py
+++ notebooks/combine_and_split_census.py
@@ -1,29 +1,34 @@
 import scanpy
 import anndata
 import glob 
+import numpy as np
 
 
 def load_rename_adata(file_path: str) -> anndata.AnnData:
 	adata = scanpy.read_h5ad(file_path)
 	adata.obs_names = adata.obs.soma_joinid.astype(str)
+	adata.var_names = adata.var.feature_id.astype(str)
 	return adata
 
 
 file_paths = glob.glob("./notebooks/census_chunks/*.h5ad")
 
-adata_list = [load_rename_adata(fp) for fp in file_paths[:]]  # Limit to first 1000 files for testing
+adata_list = [load_rename_adata(fp) for fp in file_paths[:100]]  # Limit to first 1000 files for testing
 adata = anndata.concat(adata_list, merge="same")
 adata.obs.drop("soma_joinid", axis=1, inplace=True)
+adata.var.drop("feature_id", axis=1, inplace=True)
 
 scanpy.pp.log1p(adata, copy=False) ## Log-normalize the data inplace
 
 ## Split samples in 5% for tuning, 5% as holdout, 90% for training
 
-holdout_names, holdout_idx = scanpy.pp.subsample(adata.obs_names, fraction=0.05, copy=True, random_state=42)
-tune_names, tune_idx = scanpy.pp.subsample(adata.obs_names.difference(holdout_names), fraction=0.05, copy=True, random_state=42)  
-train_names = adata.obs_names.difference(holdout_names.union(tune_names))
+holdout_names, holdout_idx = scanpy.pp.sample(np.array(adata.obs_names), fraction=0.05, copy=True, rng=42)
+tune_names, tune_idx = scanpy.pp.sample(np.array(adata.obs_names.difference(holdout_names)), fraction=0.05, copy=True, rng=42)  
+# combine numpy arrays using numpy's union1d (works with numpy.ndarray)
+combined = np.union1d(holdout_names, tune_names)
+train_names = adata.obs_names.difference(combined)
 
 ## Save splits to h5ad files
-scanpy.write("./notebooks/large_sc_data/census_train_split.h5ad", adata[train_names])
-scanpy.write("./notebooks/large_sc_data/census_tune_split.h5ad", adata[tune_names])
-scanpy.write("./notebooks/large_sc_data/census_holdout_split.h5ad", adata[holdout_names])
+scanpy.write("./large_sc_data/census_train_split.h5ad", adata[train_names])
+scanpy.write("./large_sc_data/census_tune_split.h5ad", adata[tune_names])
+scanpy.write("./large_sc_data/census_holdout_split.h5ad", adata[holdout_names])

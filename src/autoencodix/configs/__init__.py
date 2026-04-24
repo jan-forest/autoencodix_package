7844528f45a536faf8b52f8a10d6fae52b961cf2
ChristianBaeuchl <christian.baeuchl83@gmail.com>
2026-04-24 15:46:47 +0200

    included imagix3dconfig class

--- src/autoencodix/configs/__init__.py
+++ src/autoencodix/configs/__init__.py
@@ -6,6 +6,7 @@ from .vanillix_config import VanillixConfig
 from .varix_config import VarixConfig
 from .xmodalix_config import XModalixConfig
 from .maskix_config import MaskixConfig
+from .imagix3d_config import Imagix3DConfig
 
 __all__ = [
     "DataCase",
@@ -18,4 +19,5 @@ __all__ = [
     "VarixConfig",
     "XModalixConfig",
     "MaskixConfig",
+    "Imagix3DConfig"
 ]

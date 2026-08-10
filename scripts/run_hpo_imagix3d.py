17513bc210b114b5f2deefc6748ce48e87502452
ChristianBaeuchl <christian.baeuchl83@gmail.com>
2026-08-10 22:09:56 +0200

    new settings

--- scripts/run_hpo_imagix3d.py
+++ scripts/run_hpo_imagix3d.py
@@ -193,7 +193,7 @@ def run_synetune_hpo(
         "train_normalization": "group",
         "keep_mu_positive": 0,
         "batch_size": 48,
-        "hidden_dim": 16,
+        #"hidden_dim": 16,
 
         # Hardware params
         "device": "cuda",
@@ -201,11 +201,11 @@ def run_synetune_hpo(
 
         # Tunable params
         #"batch_size": choice([16, 32, 48]),
-        "learning_rate": loguniform(1e-6, 1e-3),
-        "weight_decay": loguniform(1e-6, 1e-2),
-        "beta": loguniform(1e-6, 5e-2),
+        "learning_rate": loguniform(1e-7, 1e-3),
+        "weight_decay": loguniform(1e-7, 1e-2),
+        "beta": loguniform(1e-7, 5e-2),
         "latent_dim": choice([16, 32, 48, 64, 96, 128]),
-        #"hidden_dim": choice([16, 32, 48, 64]),
+        "hidden_dim": choice([16, 32, 48, 64]),
         #"train_normalization": choice(["group", "instance", "batch"]),
         # "anneal_function": choice(
         #     [
@@ -224,11 +224,11 @@ def run_synetune_hpo(
     points_to_evaluate = [
         {
             #"batch_size": 32,
-            "learning_rate": 0.0006,
-            "weight_decay": 0.003,
-            "beta": 0.0000015,
-            "latent_dim": 32,
-            #"hidden_dim": 16,
+            "learning_rate": 0.000556,
+            "weight_decay": 0.0000015,
+            "beta": 0.00007,
+            "latent_dim": 16,
+            "hidden_dim": 16,
             #"anneal_function": "logistic-late",
             #"train_normalization": "group",
             #"keep_mu_positive": 0,

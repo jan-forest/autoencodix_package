700d2d456f64617d64f304a8b9856556127d2f41
ChristianBaeuchl <christian.baeuchl83@gmail.com>
2026-09-10 13:10:59 +0200

    setup hpo for cta data

--- scripts/run_hpo_imagix3d.py
+++ scripts/run_hpo_imagix3d.py
@@ -210,11 +210,11 @@ def run_synetune_hpo(
 
         # Tunable params
         # "batch_size": choice([16, 32, 48]),
-        "learning_rate": loguniform(1e-5, 3e-3),
-        "weight_decay": loguniform(1e-6, 3e-2),
-        "beta": loguniform(1e-7, 1e-2),
-        "latent_dim": choice([32, 48, 64, 80, 96, 112]),
-        "hidden_dim": choice([16, 32, 48]),
+        "learning_rate": loguniform(1e-6, 1e-3),
+        "weight_decay": loguniform(1e-7, 5e-3),
+        "beta": loguniform(1e-8, 5e-2),
+        "latent_dim": choice([32, 48, 64, 80, 96]),
+        "hidden_dim": choice([16, 32]),
         "train_normalization": choice(["group", "instance", "batch"]),
         # "anneal_function": choice(
         #     [
@@ -227,20 +227,20 @@ def run_synetune_hpo(
         #     ]
         # ),
         # Encoded as scalar values for Syne Tune compatibility
-        "keep_mu_positive": 0,
+        "keep_mu_positive": choice([0, 1])
     }
 
     points_to_evaluate = [
         {
             #"batch_size": 32,
-            "learning_rate": 0.001,
-            "weight_decay": 0.001,
-            "beta": 0.05,
+            "learning_rate": 0.000006974693167512662,
+            "weight_decay": 0.0013310102065272,
+            "beta": 0.0004866691463641,
             "latent_dim": 48,
             "hidden_dim": 16,
             #"anneal_function": "logistic-late",
-            "train_normalization": "batch",
-            #"keep_mu_positive": 0,
+            "train_normalization": "group",
+            "keep_mu_positive": 1,
         }
     ]
 
